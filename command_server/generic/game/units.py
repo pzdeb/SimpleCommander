2e408de1982a265a0cac7b0039acd10bf1abc835
Volodymyr Trotsyshyn <devova@gmail.com>
2015-09-16 23:42:22 +0300

    base unit classes

--- command_server/generic/game/units.py
+++ command_server/generic/game/units.py
@@ -1,12 +1,12 @@
 import asyncio
 from collections import namedtuple
 
-Position = namedtuple('Position', ['x', 'y'])
+Position2D = namedtuple('Position2D', ['x', 'y'])
+Position3D = namedtuple('Position3D', ['x', 'y', 'z'])
 
 
 class BaseUnit(object):
     name = 'Unit'
-    position = Position(x=0, y=0)
     speed_period = 300 #miliseconds needs to update state
 
     def __init__(self, game):
@@ -26,3 +26,21 @@ class BaseUnit(object):
 
     def destroy(self):
         self._timer.cancel()
+
+    def trigger(self, state):
+        pass
+
+class Positioned2DMixin(object):
+    position = Position2D(x=0, y=0)
+
+
+class Positioned3DMixin(object):
+    position = Position3D(x=0, y=0)
+
+
+class Unit2D(Positioned2DMixin, BaseUnit):
+    pass
+
+
+class Unit3D(Positioned3DMixin, BaseUnit):
+    pass
