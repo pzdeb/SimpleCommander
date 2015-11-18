227cb1994268587111a6f6655e179e5ddf97973e
ntodosiychuk <nata.todosiy@gmail.com>
2015-11-18 13:40:45 +0000

    Separate templates for game and visit pages.

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,5 @@
-var theGame = null;
-
-function init() {
+function init_game(gamer) {
     var canvas = document.getElementById("gameCanvas");
-    var form = document.getElementById("formStartGame");
-    var name = document.getElementById("heroName");
-    theGame = new GameController(canvas);
-
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        if (!theGame.socket){
-            theGame.createConnection(name.value)
-        }
-        else{
-            theGame.sendToServer('set_name', name.value)
-        }
-
-    }, false)
-
-}
+    var theGame = new GameController(canvas);
+    theGame.createConnection(gamer);
+};
